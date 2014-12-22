e619fd7af70f61b59258b0ec969d7378eadd7485
Daniel Siechniewicz / nullDowntime Ltd <daniel@nulldowntime.com>
2014-12-22 10:58:21 +0000

    check_disks_snmp.pl:   Applied changes by Stanislav German-Evtushenko <ginermail@gmail.com> :   1) add SNMP v2c version option (works faster usually and better for      networks with high latency)   2) add performance data output option   3) add three more exclusions
      + additional cleanup

--- check_disks_snmp.pl
+++ check_disks_snmp.pl
@@ -2,10 +2,10 @@
 # BSD Licenced
 # Copyright (c) 2009, Daniel Siechniewicz (daniel@siechniewicz.com)
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright notice,
 #     this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -14,7 +14,7 @@
 #     * Neither the name of the author nor the names of its contributors may be
 #     used to endorse or promote products derived from this software without
 #     specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -32,80 +32,91 @@ use warnings FATAL => 'all';
 
 use Net::SNMP;
 use Getopt::Long;
-use vars qw($opt_P $opt_h $opt_H $opt_W $opt_C $opt_p $opt_w $opt_c $opt_t $verbose);
+use vars qw($opt_P $opt_h $opt_H $opt_W $opt_C $opt_p $opt_w $opt_c $opt_t $verbose $opt_2 $opt_f);
 
 $ENV{'PATH'}='';
-$ENV{'BASH_ENV'}=''; 
+$ENV{'BASH_ENV'}='';
 $ENV{'ENV'}='';
+
+my $snmp_version = 1;
 
 # OID table/range we're going to use here
 my $oid_table = '.1.3.6.1.4.1.2021.9.1';
 
-my %STATUSCODE = (  
-		'OK' 		=> '0',
-		'WARNING' 	=> '1',
-		'CRITICAL' 	=> '2',
-		'UNKNOWN' 	=> '3',
-		);
+my %STATUSCODE = (
+    'OK'    => '0',
+    'WARNING'   => '1',
+    'CRITICAL'  => '2',
+    'UNKNOWN'   => '3',
+    );
 
 # Help message
 sub print_help {
-	print <<EOT
-check_disk_snmp - this script monitors diskspace and inode usage via snmp. 
+  print <<EOT
+check_disk_snmp - this script monitors diskspace and inode usage via snmp.
 You need "includeAllDisks <number>%" directive in your snmpd.conf. (substitute
 number 1-99 for <number>, but this value has no meaning for this script)
 Basic usage:
   check_disk_snmp -H hostname
-Advanced usage: 
-  check_disk_snmp -H hostname -p community -w 88 -c 93 -W 78 -C 87 -P 162 -t 20 -v 
+Advanced usage:
+  check_disk_snmp -H hostname -p community -w 88 -c 93 -W 78 -C 87 -P 162 -t 20 -v
 Available options:
          -H <hostname|ip> | --hostname=<hostname|ip>
-		your monitored host (required)
+    your monitored host (required)
          -p community | --community=<community>
-		your snmp community (default: public)
+    your snmp community (default: public)
          -P <number> | --port=<number>
-		snmp port number (default: 161)
-         -t <number> | --timeout=<number> (default: 15)
-		timeout - die after this many seconds
+    snmp port number (default: 161)
+         -T <number> | --timeout=<number> (default: 15)
+    timeout - die after this many seconds
          -h | --help
-		print this help message
-         -w <number|number%> | --warning=<number|number%> 
-		diskspace usage warning threshold (default: 90)
-         -c <number|number%> | --critical=<number|number%> 
-		diskspace usage critical threshold (default: 95)
-         -W <number|number%> | --inodewarn=<number|number%> 
-		inode usage warning threshold (default: 90)
-         -C <number|number%> | --inodecrit=<number|number%> 
-		inode usage critical threshold (default: 95)
+    print this help message
+         -w <number|number%> | --warning=<number|number%>
+    diskspace usage warning threshold (default: 90)
+         -c <number|number%> | --critical=<number|number%>
+    diskspace usage critical threshold (default: 95)
+         -W <number|number%> | --inodewarn=<number|number%>
+    inode usage warning threshold (default: 90)
+         -C <number|number%> | --inodecrit=<number|number%>
+    inode usage critical threshold (default: 95)
          -v | --verbose
-		print additional information
+    print additional information
+         -2 | --v2c
+    use snmp v2c (default: v1)
+         -f | --perfdata
+    add performance output
 EOT
 
 }
 
 
 sub usage {
-        my $format=shift;
-        printf($format,@_);
-        exit $STATUSCODE{'UNKNOWN'};
+  my $format=shift;
+  printf($format,@_);
+  exit $STATUSCODE{'UNKNOWN'};
 }
 
 # Process options
 Getopt::Long::Configure('bundling');
 GetOptions (
-	"v"   => \$verbose, "verbose"    => \$verbose,
-	"P=s" => \$opt_P, "port=s"     => \$opt_P,
-	"T=s" => \$opt_t, "timeout=s"     => \$opt_t,
-	"h"   => \$opt_h, "help"       => \$opt_h,
-	"w=s" => \$opt_w, "warning=s"  => \$opt_w,
-	"c=s" => \$opt_c, "critical=s" => \$opt_c,
-	"W=s" => \$opt_W, "inodewarn=s" => \$opt_W,
-	"C=s" => \$opt_C, "inodecrit=s" => \$opt_C,
-	"p=s" => \$opt_p, "community=s"    => \$opt_p,
-	"H=s" => \$opt_H, "hostname=s" => \$opt_H,
-	);
+  "v"   => \$verbose, "verbose"   => \$verbose,
+  "P=s" => \$opt_P, "port=s"      => \$opt_P,
+  "T=s" => \$opt_t, "timeout=s"   => \$opt_t,
+  "h"   => \$opt_h, "help"        => \$opt_h,
+  "w=s" => \$opt_w, "warning=s"   => \$opt_w,
+  "c=s" => \$opt_c, "critical=s"  => \$opt_c,
+  "W=s" => \$opt_W, "inodewarn=s" => \$opt_W,
+  "C=s" => \$opt_C, "inodecrit=s" => \$opt_C,
+  "p=s" => \$opt_p, "community=s" => \$opt_p,
+  "H=s" => \$opt_H, "hostname=s"  => \$opt_H,
+  "2"   => \$opt_2, "v2c"         => \$opt_2,
+  "f"   => \$opt_f, "perfdata"    => \$opt_f,
+  );
 
 if ($opt_h) {print_help(); exit $STATUSCODE{'OK'};}
+
+# SNMP version
+if ($opt_2) {$snmp_version = 2;}
 
 # Options checking
 
@@ -114,7 +125,7 @@ if ($opt_h) {print_help(); exit $STATUSCODE{'OK'};}
 my $hostname = $1 if ($opt_H =~ /^([-.A-Za-z0-9]+\$?)$/);
 ($hostname) || usage("Invalid host: $opt_H\n");
 
-# Community 
+# Community
 ($opt_p) || ($opt_p = shift) || ($opt_p = "public");
 my $community = $1 if ($opt_p =~ /(.*)/);
 
@@ -148,116 +159,128 @@ my $snmp_port = $1 if ($opt_P =~ /^([0-9]{1,5})$/ && $opt_P < 65535);
 my $timeout = $1 if ($opt_t =~ /^([0-9]{1,3})$/);
 ($timeout) || usage("Invalid timeout value: $opt_t\n");
 
-
 usage("Diskspace warning threshold: $opt_w greater or equal critical "
-		. "threshold: $opt_c\n") 	if ($opt_w >= $opt_c);
+    . "threshold: $opt_c\n")  if ($opt_w >= $opt_c);
 usage("Inode warning threshold: $opt_W greater or equal critical "
-		. "threshold: $opt_C\n")	if ($opt_W >= $opt_C);
-
+    . "threshold: $opt_C\n")  if ($opt_W >= $opt_C);
 
 my $state = 'OK';
 
 # Set up timeout in case something goes wrong
 $SIG{'ALRM'} = sub {
-    print "ERROR: No snmp response from $hostname (sigALRM)\n";
-    exit($STATUSCODE{"UNKNOWN"});
+  print "ERROR: No snmp response from $hostname (sigALRM)\n";
+  exit($STATUSCODE{"UNKNOWN"});
 };
 
-alarm($timeout);                    
+alarm($timeout);
 
 # Initiate snmp session
 my ($session, $error) = Net::SNMP->session(
-	-hostname  	=> $hostname,
-	-port		=> $snmp_port,
-	-community 	=> $community,
+  -hostname  => $hostname,
+  -port      => $snmp_port,
+  -community => $community,
+  -version   => $snmp_version,
 );
 
 # Return unknown if unsuccessful
 if (!defined $session) {
-	printf "UNKNOWN: %s.\n", $error;
-	exit($STATUSCODE{"UNKNOWN"});
+  printf "UNKNOWN: %s.\n", $error;
+  exit($STATUSCODE{"UNKNOWN"});
 }
 
-# Read whole table. Could be optimized to only get subtables 
+# Read whole table. Could be optimized to only get subtables
 # needed (2, 3, 9, 10)
 my $result = $session->get_table(
-                          -baseoid          => $oid_table,
+                          -baseoid => $oid_table,
                        );
 
 # Return uknown if there's no snmp query result
 if (!defined $result) {
-	printf "UNKNOWN: %s.\n", $session->error();
-	$session->close();
-	exit($STATUSCODE{"UNKNOWN"});
+  printf "UNKNOWN: %s.\n", $session->error();
+  $session->close();
+  exit($STATUSCODE{"UNKNOWN"});
 }
 
 my @real_disks;
 
-# Iterate over fstype (sub)table and exclude certain fstypes 
+# Iterate over fstype (sub)table and exclude certain fstypes
 # Tested on Debian, RedHat and FreeBSD, you might need to add
 # more(!).
 foreach my $oid (keys %$result) {
-	#print "OID: $oid\n";
-	if ( $oid =~ /^\Q$oid_table.3.\E/ ) {
-		push @real_disks, $oid unless $$result{$oid} =~ 
-			/^(?:none|sunrpc|usbfs|nfsd|proc|devpts|udev|sysfs|devfs)$/;
-	}
+  #print "OID: $oid\n";
+  if ( $oid =~ /^\Q$oid_table.3.\E/ ) {
+    push @real_disks, $oid unless $$result{$oid} =~
+      /^(?:none|sunrpc|usbfs|nfsd|proc|devpts|udev|sysfs|devfs|rpc_pipefs|binfmt_misc|fusectl)$/;
+  }
 }
-
 
 my %disk_h;
 
 # Iterate over real disks now, extracting path, diskspace and inodes utilization, etc.
 foreach my $disk_oid (@real_disks) {
 
-	(my $path_oid = $disk_oid) =~ s/^\Q$oid_table.3.\E/$oid_table.2./;
-	(my $diskspace_oid = $disk_oid) =~ s/^\Q$oid_table.3.\E/$oid_table.9./;
-	(my $inode_oid = $disk_oid) =~ s/^\Q$oid_table.3.\E/$oid_table.10./;
-	(my $index = $disk_oid ) =~ s/^.*\.//g;
+  (my $path_oid = $disk_oid) =~ s/^\Q$oid_table.3.\E/$oid_table.2./;
+  (my $diskspace_oid = $disk_oid) =~ s/^\Q$oid_table.3.\E/$oid_table.9./;
+  (my $inode_oid = $disk_oid) =~ s/^\Q$oid_table.3.\E/$oid_table.10./;
+  (my $index = $disk_oid ) =~ s/^.*\.//g;
 
-	my $status = 'OK';
-	my $status_string = 'Disk: ' . $$result{$path_oid} .' ';
+  my $status = 'OK';
+  my $status_string = 'Disk: ' . $$result{$path_oid} .' ';
+  my $perfdata = '';
 
-	if ($$result{$diskspace_oid} >= $diskspace_crit) {
-	 	$status = 'CRITICAL';
-		$status_string .= 'diskpace crit: ' . $$result{$diskspace_oid} . '%';
-	} elsif ($$result{$diskspace_oid} >= $diskspace_warn) {
-		$status = 'WARNING';
-		$status_string .= 'diskpace warn: ' . $$result{$diskspace_oid} . '%';
-	} else {
-		$status_string .= 'diskspace ok';
-	}
+  if ($$result{$diskspace_oid} >= $diskspace_crit) {
+    $status = 'CRITICAL';
+    $status_string .= 'diskpace crit: ' . $$result{$diskspace_oid} . '%';
+  } elsif ($$result{$diskspace_oid} >= $diskspace_warn) {
+    $status = 'WARNING';
+    $status_string .= 'diskpace warn: ' . $$result{$diskspace_oid} . '%';
+  } else {
+    $status_string .= 'diskspace ok';
+  }
 
-	if ($$result{$inode_oid} >= $inodes_crit) {
-		$status = 'CRITICAL';
-		$status_string .= ', inodes crit: ' . $$result{$inode_oid} ."%.\n";
-	} elsif ($$result{$inode_oid} >= $inodes_warn) {
-		$status = 'WARNING' unless ( $status eq 'CRITICAL' );
-		$status_string .= ', inodes warn: ' . $$result{$inode_oid} . "%.\n";
-	} else {
-		$status_string .= ", inodes ok.";
-	}
-	
-	%{$disk_h{$index}} = (
-			'diskspace_used' 	=> $$result{$diskspace_oid},
-			'inodes_used'		=> $$result{$inode_oid},
-			'path'			=> $$result{$path_oid},
-			'dev'			=> $$result{$disk_oid},
-			'status'		=> $status,
-			'status_string'		=> $status_string,
-		) ;
+  $perfdata .= $result{$path_oid} . "_(space)" . "=" . $result{$diskspace_oid}
+              . "%" . ";" . $diskspace_warn . ";" . $diskspace_crit
+              . ";0;100";
+
+  if ($$result{$inode_oid} >= $inodes_crit) {
+    $status = 'CRITICAL';
+    $status_string .= ', inodes crit: ' . $$result{$inode_oid} ."%.\n";
+  } elsif ($$result{$inode_oid} >= $inodes_warn) {
+    $status = 'WARNING' unless ( $status eq 'CRITICAL' );
+    $status_string .= ', inodes warn: ' . $$result{$inode_oid} . "%.\n";
+  } else {
+    $status_string .= ", inodes ok.";
+  }
+
+  $perfdata .= " " . $result{$path_oid} . "_(inodes)" . "="
+              . $result{$inode_oid} . "%" . ";" . $inodes_warn
+              . ";" . $inodes_crit . ";0;100";
+
+  %{$disk_h{$index}} = (
+    'diskspace_used' => $$result{$diskspace_oid},
+    'inodes_used'    => $$result{$inode_oid},
+    'path'           => $$result{$path_oid},
+    'dev'            => $$result{$disk_oid},
+    'status'         => $status,
+    'status_string'  => $status_string,
+  );
 }
 
 my $state_string;
+my $perf_string;
 
 foreach my $disk (sort {$a <=> $b} (keys(%disk_h))) {
-	next if (!$verbose && $disk_h{$disk}{'status'} =~ /^OK$/ );
-	$state = $disk_h{$disk}{'status'} unless ($state eq 'CRITICAL');
-	$state_string .= $disk_h{$disk}{'status_string'};
+  $perf_string .= $disk_h{$disk}{'perfdata'} . " ";
+  next if (!$verbose && $disk_h{$disk}{'status'} =~ /^OK$/ );
+  $state = $disk_h{$disk}{'status'} unless ($state eq 'CRITICAL');
+  $state_string .= $disk_h{$disk}{'status_string'};
 }
 
 print $state . ": ";
 print $state_string || "All disks within limits";
 print "\n";
+if ($opt_f) {
+  print '|' . $perf_string;
+  print "\n";
+}
 exit $STATUSCODE{$state};
-
